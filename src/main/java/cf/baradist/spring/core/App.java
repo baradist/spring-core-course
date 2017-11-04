e28f1e90479431069a5a5d09a74ce709de76441e
Oleg Grigoryev <oleg_grigoryev@epam.com>
2017-11-05 00:19:47 +0300

    1.3 Bean Naming and Context Start-up

--- src/main/java/cf/baradist/spring/core/App.java
+++ src/main/java/cf/baradist/spring/core/App.java
@@ -2,15 +2,23 @@ package cf.baradist.spring.core;
 
 import cf.baradist.spring.core.beans.Client;
 import cf.baradist.spring.core.loggers.EventLogger;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Data
+@AllArgsConstructor
 public class App {
     private Client client;
     private EventLogger eventLogger;
 
     public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
 
+        app.logEvent("Some event for 1");
+        app.logEvent("Some event for 2");
     }
 
     public void logEvent(String msg) {
