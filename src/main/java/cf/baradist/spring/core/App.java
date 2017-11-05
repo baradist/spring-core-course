539eab9cf7027dcb958979aba2e8e9d199b832a1
Oleg Grigoryev <oleg_grigoryev@epam.com>
2017-11-06 00:07:24 +0300

    1.5 Initialize and Destroy: CachedFileEventLogger

--- src/main/java/cf/baradist/spring/core/App.java
+++ src/main/java/cf/baradist/spring/core/App.java
@@ -5,7 +5,7 @@ import cf.baradist.spring.core.loggers.Event;
 import cf.baradist.spring.core.loggers.EventLogger;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Data
@@ -13,14 +13,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     private Client client;
     private EventLogger eventLogger;
-    private static ApplicationContext ctx;
+    private static ConfigurableApplicationContext ctx;
 
     public static void main(String[] args) {
         ctx = new ClassPathXmlApplicationContext("spring.xml");
         App app = (App) ctx.getBean("app");
 
-        app.logEvent("Some event for 1");
-        app.logEvent("Some event for 2");
+        app.logEvent("Message 1");
+        app.logEvent("Message 2");
+        app.logEvent("Message 3");
+        app.logEvent("Message 4");
+        app.logEvent("Message 5");
+        app.logEvent("Message 6");
+        app.logEvent("Message 7");
+        app.logEvent("Message 8");
+        app.logEvent("Message 9");
+
+        ctx.close();
     }
 
     public void logEvent(String msg) {
---
+++ src/main/java/cf/baradist/spring/core/loggers/CachedFileEventLogger.java
@@ -0,0 +1,50 @@
+package cf.baradist.spring.core.loggers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CachedFileEventLogger extends FileEventLogger {
+    public static final int DEFAULT_CASHE_SIZE = 100;
+    private int casheSize;
+    private List<Event> cashe;
+
+    public CachedFileEventLogger(String fileName) {
+        this(fileName, DEFAULT_CASHE_SIZE);
+    }
+
+    public CachedFileEventLogger(String fileName, int casheSize) {
+        super(fileName);
+        this.casheSize = casheSize;
+        cashe = new ArrayList<>(casheSize);
+    }
+
+    @Override
+    public void init() throws IOException {
+        super.init();
+    }
+
+    public void destroy() {
+        writeEventsFromAndClearCashe();
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        cashe.add(event);
+        if (cashe.size() == casheSize) {
+            writeEventsFromAndClearCashe();
+        }
+    }
+
+    private void writeEventsFromAndClearCashe() {
+        if (cashe.isEmpty()) {
+            return;
+        }
+        String string = cashe.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("\n"));
+        writeStringToFile(string);
+        cashe.clear();
+    }
+}
